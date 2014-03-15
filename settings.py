1376e2b5ee66fa870554191f74ddf8c5ef69ddbd
Scopel Emanuele <scopel.emanuele@silvergeko.it>
2014-03-15 22:02:50 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
-
 # Your Key
-HoneyKey = 'vzfzfjokvrar'
+HoneyKey = 'asdfghjkl'
 # Honeypot test url
 DNSUrl = 'dnsbl.httpbl.org'
 # redirect spammer to...
-HoneyPotUrl = 'http://www.mollichina.com/mumplayer.php?page=80'
+HoneyPotUrl = 'http://www.quicklink.it'
 # Permit Suspicious
 Suspicious = True
 # Maximum threat rating permitted http://www.projecthoneypot.org/threat_info.php
